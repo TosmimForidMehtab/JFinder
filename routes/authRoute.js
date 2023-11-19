a890438db16f0f59afcbb5244a59c2ea550f217a
Tosmim <tosmimforidmehtab@gmail.com>
2023-11-19 15:59:54 +0530

    Added security

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,16 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 import { login, register } from "../controllers/authController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.post("/register", register);
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10,
+    message:
+        "Too many requests from this IP, please try again after 15 minutes",
+});
+
+router.post("/register", apiLimiter, register);
 router.post("/login", authMiddleware, login);
 export default router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 import cors from "cors";
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
 import jobRoutes from "./routes/jobRoutes.js";
@@ -11,6 +14,9 @@ connectDB();
 
 const app = express();
 
+app.use(helmet()); // Set security for HTTP headers
+app.use(xss()); // Sanitize user input and prevent XSS attacks
+app.use(mongoSanitize()); // Sanitize user input and prevent MongoDB query injection
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
